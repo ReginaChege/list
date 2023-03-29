b7525c98a4f769f1065a7a1099fb4dcf84efe3b6
ReginaChege <reginachegewairimu@gmail.com>
2023-03-29 17:04:35 +0300

    lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 import javax.management.MBeanRegistration
 
+
+fun main(){
+    //Given a list of Person objects, each with the attributes, name, age,
+//height and weight. Sort the list in order of descending age
+    var person=("")
+measurements()
+Car()
+}
 //Given a list of 10 strings, write a function that returns a list of the strings
 //at even indices i.e index 2,4,6 etc
-fun main(){
-measurements()
-Car("KVS 659V",90987.89)
-    Car("KVS 659V",23456.89)
-    Car("KVS 659V",123455.89)
-    Car("KVS 659V",876547.89)
-
-}
 //fun dinner(items:){
 //    var item= listOf("coster","soup spoon","desert spoon","fork","napkin","plates","glass","wine","beer","desert")
 //  var itemindex=
@@ -22,20 +22,16 @@ fun measurements(){
     println(height.average())
     println( height.sum())
 }
-//Given a list of Person objects, each with the attributes, name, age,
-//height and weight. Sort the list in order of descending age
-//fun personObject(name:String,age:Int,height:Double,weight:Double){
-//var person= listOf("Regina",20,175.0,65.0)
-//    var sortperson=person.
-//}
 //Given a list similar to the one above, write a function in which you will
 //create 2 more people objects and add them to the list at one go.
 
 //Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of
 //all the vehicles in the list.
-fun Car(registration:String,milage:Double,):Double{
- var carmilage= listOf(27900.9,290000.89,76890.76,500789.90,67890.534,12345.90,765489.90)
-    return carmilage.average()
-    println()
+fun Car(){
+ var carmilage= listOf(90987.89,23456.89,123455.89,876547.89)
+     println( carmilage.average())
 }
+//Given a list of Person objects, each with the attributes, name, age,
+//height and weight. Sort the list in order of descending age
+fun persons(name:String)
